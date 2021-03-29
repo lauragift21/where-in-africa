b620a22b2dff3c9eaad5211ae588eac8aef7ceb2
Gift Egwuenu <laurandidi21@gmail.com>
2021-03-29 05:49:18 +0200

    composable change

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,27 +1,25 @@
 <template>
- <div class="relative my-8">
-  <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-   <svg
-    fill="none"
-    stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="2"
-    viewBox="0 0 24 24"
-    class="w-6 h-6"
-   >
-    <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-   </svg>
-  </span>
-  <input
-   type="text"
-   name="search"
-   v-model="search"
-   placeholder="Search for a Country..."
-   class="pl-10 w-full md:w-2/3 lg:w-1/3 border-2 border-gray-100 py-2 shadow-md"
-  />
- </div>
- <div class="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+ <span class="relative inset-y-0 left-0 top-8 flex items-center pl-2">
+  <svg
+   fill="none"
+   stroke="currentColor"
+   stroke-linecap="round"
+   stroke-linejoin="round"
+   stroke-width="2"
+   viewBox="0 0 24 24"
+   class="w-6 h-6"
+  >
+   <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+  </svg>
+ </span>
+ <input
+  type="text"
+  name="search"
+  v-model="search"
+  placeholder="Search for a Country..."
+  class="pl-10 w-full md:w-2/3 lg:w-1/3 border-2 border-gray-100 py-2 shadow-md"
+ />
+ <div class="mx-auto mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
   <span v-if="loading">loading...</span>
   <div v-for="country in searchList" :key="country.id" v-else>
    <countries-card :country="country" />
@@ -31,7 +29,7 @@
 
 <script>
 import CountriesCard from "@/components/CountriesCard";
-import { reactive, toRefs, onMounted, computed, watch } from "vue";
+import { reactive, toRefs, onMounted, computed, watchEffect } from "vue";
 
 export default {
  components: { CountriesCard },
@@ -66,15 +64,15 @@ export default {
    });
   });
 
-  watch(() => {
-   console.log(
-    "We have about " + state.countries.length + " Countries in Africa"
-   );
-  });
-
-  // watchEffect(() => {
-  //  console.log("We have about " + state.countries.length + " Countries in Africa");
+  // watch(() => {
+  //  console.log(
+  //   "We have about " + state.countries.length + " Countries in Africa"
+  //  );
   // });
+
+  watchEffect(() => {
+   console.log("We have about " + state.countries.length + " Countries in Africa");
+  });
 
   return {
    ...toRefs(state),
